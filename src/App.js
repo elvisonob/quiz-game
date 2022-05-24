f8ac5bdb8eafab73cf0d2a878093e8ca2fa7b182
Elvis Onobhayedo <elvisonob@yahoo.com>
2022-05-24 09:56:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  const checkAnswers = function (questionObject, selectedAnswer) {
+    console.log(`You made a selection, ${selectedAnswer}`);
+    if (selectedAnswer === questionObject.correct_answer) {
+      console.log("You are correct");
+    } else {
+      console.log("You are wrong");
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log("insideFetch results", json.results);
+        setList(json.results);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz</h1>
+      {list.map((question) => {
+        console.log(question);
+        const allAnswers = [
+          ...question.incorrect_answers,
+          question.correct_answer,
+        ];
+        shuffleArray(allAnswers);
+        return (
+          <div className="question">
+            <p>{question.question}</p>
+            {allAnswers.map((option) => {
+              return (
+                <button onClick={() => checkAnswers(question, option)}>
+                  {option}
+                </button>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
